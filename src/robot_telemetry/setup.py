c6a58d83a68faae7473c8112f336476b1115aa4a
Tejal Goyal <f20251726@pilani.bits-pilani.ac.in>
2026-09-23 12:35:59 +0000

    updated

--- src/robot_telemetry/setup.py
+++ src/robot_telemetry/setup.py
@@ -1,9 +1,12 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup 
+#It installs 2 packages findpackage - finds the python packages and setup - creates and install the packages
 from glob import glob
+#search for files and pathnames.It uses matching pattern instaead of using full expressions.
 import os
-
+#helps create file paths.
 package_name = 'robot_telemetry'
 
+#creating the package
 setup(
     name=package_name,
     version='0.0.0',
@@ -22,17 +25,13 @@ setup(
             glob('launch/*.py')
         ),
     ],
-    install_requires=['setuptools'],
-    zip_safe=True,
+    install_requires=['setuptools'], #prerequisite required for the package 
+    zip_safe=True, #optional
     maintainer='tejal',
-    maintainer_email='tejal@todo.todo',
-    description='TODO: Package description',
+    maintainer_email='tejal@todo.todo', #temporary placeholder...
+    description='ROS 2 package for robot telemetry communication.', #can write TODO too
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    #creates executable commands
     entry_points={
         'console_scripts': [
 		'robot_controller = robot_telemetry.robot_controller:main',
